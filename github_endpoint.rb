f0f3d17db5de1a60cecff9e1638f692fdcf9e2c5
John Metta <john@johnmetta.com>
2016-06-17 14:00:30 -0700

    bump

--- github_endpoint.rb
+++ github_endpoint.rb
@@ -4,5 +4,5 @@ require './slacker'
 
 post '/payload' do
   push = JSON.parse(request.body.read)
-  Slacker.new.post "I got some JSON", push.inspect
+  Slacker.new.post "I got some JSON", push.inspect.to_s
 end
