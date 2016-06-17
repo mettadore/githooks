fe7889ceaf54d391bf893fcf8e6f5bdd2836ecc7
John Metta <john@johnmetta.com>
2016-06-17 14:30:54 -0700

    bump

--- github_endpoint.rb
+++ github_endpoint.rb
@@ -4,5 +4,20 @@ require './slacker'
 
 post '/payload' do
   push = JSON.parse(request.body.read)
-  Slacker.new.post "I got some JSON", push.inspect.to_s
+  icon_url = 'https://upload.wikimedia.org/wikipedia/en/a/a6/Bender_Rodriguez.png'
+  attachment = [{
+            fallback: opts[:attach][:fallback] || 'Attachment',
+            color: opts[:attach][:color] || 'good',
+            text: opts[:attach][:text],
+            pretext: opts[:attach][:pretext],
+            fields: format_fields(opts[:attach][:fields])
+        }]
+  attachments = push.map do |k,v|
+    {
+      fallback: 'Attachment',
+      color: 'good',
+      text: k
+      fields: v
+    }
+  Slack::Notifier.ping "Github Payload", icon_url: icon_url, attachments: attachments
 end
