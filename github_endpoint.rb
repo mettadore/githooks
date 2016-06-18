2b761a34b558a843d9f935c3d83d2dd496c0a93d
John Metta <john@johnmetta.com>
2016-06-18 14:43:55 -0700

    bump

--- github_endpoint.rb
+++ github_endpoint.rb
@@ -8,9 +8,9 @@ def format_fields(fields)
     when Array
       fields
     when Hash
-      fields.map { |k, v| {title: k.to_s, value: v.to_s} }
+      fields.map { |k, v| { title: k.to_s, value: v.to_s } }
     else
-      [{title: fields.class.name, value: fields.to_s}]
+      [{ title: fields.class.name, value: fields.to_s }]
   end
 end
 
@@ -18,7 +18,7 @@ post '/payload' do
   push = JSON.parse(request.body.read)
   icon_url = 'https://upload.wikimedia.org/wikipedia/en/a/a6/Bender_Rodriguez.png'
   slack = Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL'])
-  attachments = push.map do |k,v|
+  attachments = push.map do |k, v|
     next unless %w{action pull_request}.include? k
     {
       fallback: 'Attachment',
@@ -30,8 +30,8 @@ post '/payload' do
   #if push[:action] == 'opened'
   #  && push[:pull_request].present?
   #  && push[:pull_request][:title].match(/QMS/)
-  attachments = {action: push[:action], pull: push[:pull_request].present?, title: push[:pull_request][:title]}
-    slack.ping "Github Payload", icon_url: icon_url, attachments: attachments    client = Octaclient.new(push[:repository][:full_name])
+  attachments = { action: push[:action], pull: push[:pull_request].present?, title: push[:pull_request][:title] }
+  slack.ping "Github Payload", icon_url: icon_url, attachments: attachments client = Octaclient.new(push[:repository][:full_name])
   #  client.label!(push[:pull_request][:number])
-  end
+
 end
