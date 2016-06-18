4b481f9136f167dc61b640d5c76e71710e6dea46
John Metta <john@johnmetta.com>
2016-06-18 15:16:29 -0700

    bump

--- github_endpoint.rb
+++ github_endpoint.rb
@@ -18,7 +18,7 @@ post '/payload' do
   push = JSON.parse(request.body.read)
   icon_url = 'https://upload.wikimedia.org/wikipedia/en/a/a6/Bender_Rodriguez.png'
   slack = Slack::Notifier.new(ENV['SLACK_WEBHOOK_URL'])
-  slack.ping push[:action], icon_url: icon_url
+  slack.ping 'Test', icon_url: icon_url
 
   attachments = push.select { |k, _v| %w{action pull_request}.include?(k) }.map do |k, v|
     {
